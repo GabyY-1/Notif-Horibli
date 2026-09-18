b2e9320651d29f65effbda9cdd8e388b9f9883f3
GabyY <gab.basque11@gmail.com>
2026-09-18 20:02:36 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL="https://vgpsfmtdenpgsoiljooy.supabase.co";
+const SUPABASE_KEY="sb_publishable_NnKn2Bxtos5J6trmqaH8zg_brt92hR1";
+const supabaseClient=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
